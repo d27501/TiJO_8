ec032ab9e9560ea34966d99a07195175cdf9e8b5
MateuszC100 <mateuszcierniak102@o2.pl>
2017-04-26 12:33:48 +0200

    Add few tests to functions and report

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,15 +5,21 @@ var app = require('../app/app');
 
 describe('reverseNumber', function ()
 {
-
+    it('should return number with reversed digits', function(){
+        expect(app.reverseNumber(16)).to.eql(61);
+    })
 });
 
 describe('returnOnlyLetter', function ()
 {
-
+    it('should return only letters from string', function(){
+        expect(app.returnOnlyLetter('56auto23')).to.eql('auto');
+    })
 });
 
 describe('isEmail', function ()
 {
-
+    it('should return true when the argument is a valid email', function(){
+        expect(app.isEmail('abcd@o2.pl')).to.eql(true);
+    })
 });
